cf5c39bef24d2c4af2464234fb32a0d78ac517dd
Kristoffer Vinther <kristoffer@zliide.com>
2022-07-14 16:42:12 +0200

    Version 1

---
+++ prepare.js
@@ -0,0 +1,18 @@
+import { writeFile } from 'fs/promises'
+
+async function main() {
+    console.error('PREPARING GIT DEPENDENCY')
+
+    await new Promise(resolve => setTimeout(resolve, 30000))
+
+    await writeFile('index.js', `export function aNumber() {
+    return 1
+}
+`)
+}
+
+main()
+    .catch(e => {
+        console.error(e)
+        process.exit(1)
+    })
